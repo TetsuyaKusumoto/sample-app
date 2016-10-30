093233f5c7acf6b25a5a9aa22ce08d9d62252219
Tetsuya Kusumoto <tetsuya.kusumoto@gmail.com>
2016-10-30 12:28:10 +0900

    modify capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,43 +1,63 @@
 # config valid only for current version of Capistrano
+
+# 基本設定
 lock '3.6.1'
-
 set :application, 'sample-app'
-set :repo_url, 'git@github.com:tetsuyakusumoto/sample-app.git'
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/var/www/sample-app'
-
-# Default value for :scm is :git
-set :scm, :git
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
 set :keep_releases, 5
+set :stages, %w(production staging)
+set :default_stage, 'staging'
+set :scm, :git
+set :repo_url, 'git@github.com:tetsuyakusumoto/sample-app.git'
+set :user, 'vagrant'
+set :use_sudo, false
+# ssh
+set :ssh_options, {
+  user: fetch(:user),
+  keys: %w(~/.ssh/id_rsa),
+  auth_methods: %w(publickey),
+  # .ssh/configの設定ファイルを使用できるようにする。これを設定することで`Host hoge`の部分で接続できることができる。
+  forward_agent: true
+}
 
 set :default_env, {
   rbenv_root: "~/.rbenv",
   path: "~/.rbenv/shims:~/.rbenv/bin:$PATH"
 }
 
+set :rbenv_ruby, '2.2.2'
 set :tmp_dir, "#{fetch(:home)}/tmp"
+
+# puma用設定
+set :puma_user, fetch(:user)
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_default_control_app, "unix://#{shared_path}/tmp/sockets/pumactl.sock"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 16]
+set :puma_workers, 0
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false
+set :puma_preload_app, false
+set :puma_plugins, []  #accept array of plugins
+set :nginx_use_ssl, false
+# capistrano3-puma の既定の再起動処理を無効化
+set :puma_default_hooks, false
+
+# puma を capistrano で利用するための設定
+set :puma_preload_app, false
+set :prune_bundler, true
+
+
+
+
+
+
+
+
